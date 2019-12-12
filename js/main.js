2a89d912571489531ca544865ade2e935093f866
KarimTazmi <57253656+KarimTazmi@users.noreply.github.com>
2019-12-12 15:25:03 +0100

    Finished pages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function initMap() {
   // Your location
   const loc = { lat: 42.361145, lng: -71.057083 };
   // Centered map on location
-  const map = new google.maps.Map(document.querySelector('.map'), {
+  const map = new google.maps.Map(document.querySelector('.contact__map'), {
     zoom: 14,
     center: loc
   });
